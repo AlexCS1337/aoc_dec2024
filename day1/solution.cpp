8b4d2e04b766d276c3cbf3c5666ac8ca18ffc374
AlexCS1337 <alexander.carlos.silva@gmail.com>
2025-01-02 15:39:26 +1000

    Finialize part 1 and start writing part2 for day1 challenge

--- day1/solution.cpp
+++ day1/solution.cpp
@@ -3,21 +3,56 @@
 #include <iostream>
 #include <fstream>
 #include <string>
+#include <vector>
+#include <cmath>
+#include <algorithm>
+#include <sstream>
+#include <chrono>
 
 int main()
 {
+  auto start = std::chrono::high_resolution_clock::now();
+
   std::string line;
   std::ifstream inputFile ("input.txt");
-  if (inputFile.is_open())
-  {
-    while ( getline (inputFile,line) )
-    {
-      std::cout << line << '\n';
-    }
-    inputFile.close();
-  }
+  std::vector<int> leftList, rightList;
+  // if (inputFile.is_open())
+  // {
+    // while ( getline (inputFile,line) )
+    // {
+      // std::cout << line << '\n';
+    // }
+    // inputFile.close();
+  // }
 
-  else std::cout << "Unable to open file"; 
+  // else std::cout << "Unable to open file";
+  while (std::getline(inputFile, line)) {
+        std::stringstream ss(line);
+        std::string left, right;
+        ss >> left >> right;
+        leftList.push_back(std::stoi(left));
+        rightList.push_back(std::stoi(right));
+    }
+
+    std::sort(leftList.begin(), leftList.end(), std::greater<int>());
+    std::sort(rightList.begin(), rightList.end(), std::greater<int>());
+
+    int total = 0;
+
+    while (!leftList.empty()) {
+        int minLeft = leftList.back();
+        leftList.pop_back();
+        int minRight = rightList.back();
+        rightList.pop_back();
+        int difference = std::abs(minRight - minLeft);
+        total += difference;
+    }
+
+    std::cout << "Total: " << total << "\n";
+
+    auto end = std::chrono::high_resolution_clock::now();
+    std::chrono::duration<double> execution_time = end - start;
+    std::cout << "Execution time: " << execution_time.count() << " seconds" << "\n";
 
   return 0;
 }
